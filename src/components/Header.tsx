c28bc7fff1edeee5d3d6a7c900ab299a2606d45e
zhuaf <2353536563@qq.com>
2019-10-01 22:50:52 +0800

    详情

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { Icon } from 'antd';
+import IonSvg from '../icons/icon'
 class Header extends React.Component{
+    
     public render(){
         return (
             <header className="app-header">
@@ -9,19 +11,19 @@ class Header extends React.Component{
                 <a href="/" className="navbar-brand text-brand">HRAN'S</a>
                 <ul className="navbar-nav mr-auto">
                   <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
-                  <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
-                  <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
-                  <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
-                  <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
-                  <li className="nav-item"><a href="/" className="nav-link">首页</a></li>
+                  <li className="nav-item"><a href="/" className="nav-link">分类</a></li>
+                  <li className="nav-item"><a href="/" className="nav-link">归档</a></li>
+                  <li className="nav-item"><a href="/" className="nav-link">关于</a></li>
+                  <li className="nav-item"><a href="/" className="nav-link">更多</a></li>
                 </ul>
               </div>
               <div className="header-right">
                 <ul className="navbar-nav">
-                  <li className="nav-item"><a href="/" className="nav-icon"><Icon type="github" theme="filled" /></a></li>
+                  <li className="nav-item"><a href="https://github.com/stogefei" target="blank" className="nav-icon"><Icon type="github" theme="filled" /></a></li>
+                  <li className="nav-item"><a href="/" className="nav-icon"><Icon type="play-square" /></a></li>
                   <li className="nav-item"><a href="/" className="nav-icon"><Icon type="chrome" /></a></li>
-                  <li className="nav-item"><a href="/" className="nav-icon"><Icon type="youtube" /></a></li>
-                  <li className="nav-item"><a href="/" className="nav-icon"><Icon type="codepen" /></a></li>
+                  <li className="nav-item"><a href="/" className="nav-icon"><Icon type="skin" /></a></li>
+                  <li className="nav-item"><a href="/" className="nav-icon"><IonSvg/></a></li>
                 </ul>
               </div>
             </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// import Loadable from 'react-loadable';
+// import * as Loadable from 'react-loadable';
 import './styles/index.less';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './views/App';
 import BlogDetail from './views/BlogDetail';
+// const BlogDetail = Loadable(() => import('./views/BlogDetail'))
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
